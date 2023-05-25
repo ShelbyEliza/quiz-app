49fe4bbeeb53a6c26b381d0a221eca727a4db93f
Shelby Shipley <shelbyelizashipley@gmail.com>
2023-05-25 14:21:15 -0700

    add icon to homepage, add spelling quiz page

---
+++ app/quizzes/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function SpellingQuiz() {
+  return (
+    <div>
+      <h1>Welcome to Spelling Quizness</h1>
+      <Link href="/">Go Back Home!</Link>
+    </div>
+  );
+}
